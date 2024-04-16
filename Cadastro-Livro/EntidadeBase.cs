36c7324e97db7b3819a08fd9f66cd93d675f31ce
Guilherme <guilherme.silva2616@etec.sp.gov.br>
2024-04-16 11:18:36 -0300

    atualização-16.04

---
+++ Cadastro-Livro/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Livro
+{
+    public abstract class EntidadeBase //Classe publica - EntidadeBase 
+    {
+        public int id { get; protected set; } //propriedade publica "id" - get(permite a leitura) - protected set (modificação apenas dentro da classe e derivados)
+    }
+}
---
+++ Cadastro-Livro/Genero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_Livro
+{
+    public enum Genero //Classe publica "Genero" - Enum vai criar uma enumeração
+    {
+
+        //Categorias de Livro
+
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        Auto_Ajuda = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
+    }
+}
